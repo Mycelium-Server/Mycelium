3441054db07c21394184754514a890a46de87822
JNNGL <jnngles@gmail.com>
2022-07-28 19:49:32 +1100

    Dust particle

---
+++ src/server/particle/dust_particle.cpp
@@ -0,0 +1,15 @@
+#include "dust_particle.h"
+
+DustParticle::DustParticle() = default;
+DustParticle::~DustParticle() = default;
+
+void DustParticle::writeData(ByteBuffer& buf) {
+    buf.writeFloat(red);
+    buf.writeFloat(green);
+    buf.writeFloat(blue);
+    buf.writeFloat(scale);
+}
+
+int DustParticle::getParticleID() const {
+    return 14;
+}
