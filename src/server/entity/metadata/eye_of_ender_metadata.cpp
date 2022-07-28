942e2e2de2fb05f596e337f4f245a7e0e20f2337
JNNGL <jnngles@gmail.com>
2022-07-28 18:48:06 +1100

    Entity metadata: Eye of ender

---
+++ src/server/entity/metadata/eye_of_ender_metadata.cpp
@@ -0,0 +1,9 @@
+#include "eye_of_ender_metadata.h"
+
+EyeOfEnderMetadata::EyeOfEnderMetadata() = default;
+EyeOfEnderMetadata::~EyeOfEnderMetadata() = default;
+
+void EyeOfEnderMetadata::wrapperWrite(MetadataBuffer& wrapper) {
+    EntityMetadata::wrapperWrite(wrapper);
+    wrapper.writeItem(8, item)
+}
