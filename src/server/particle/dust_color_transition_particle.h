70e26e6ac4ab0b8a5c2995d6ec713d239aaa2245
JNNGL <jnngles@gmail.com>
2022-07-28 19:51:43 +1100

    Dust color transition particle

---
+++ src/server/particle/dust_color_transition_particle.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "particle.h"
+
+class DustColorTransitionParticle : public AbstractParticle {
+public:
+    DustColorTransitionParticle();
+    ~DustColorTransitionParticle() override;
+
+public:
+    void writeData(ByteBuffer&) override;
+    [[nodiscard]] int getParticleID() const override;
+
+public:
+    float red = 0;
+    float green = 0;
+    float blue = 0;
+    float scale = 0.01;
+    float toRed = 0;
+    float toGreen = 0;
+    float toBlue = 0;
+
+};
