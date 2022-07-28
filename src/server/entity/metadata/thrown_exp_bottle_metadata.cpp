4b490e56d8fee8f6cf4cfff42e170117a9ae5879
JNNGL <jnngles@gmail.com>
2022-07-28 18:41:57 +1100

    Entity metadata: Thrown exp bottle

---
+++ src/server/entity/metadata/thrown_exp_bottle_metadata.cpp
@@ -0,0 +1,9 @@
+#include "thrown_exp_bottle_metadata.h"
+
+ThrownExpBottleMetadata::ThrownExpBottleMetadata() = default;
+ThrownExpBottleMetadata::~ThrownExpBottleMetadata() = default;
+
+void ThrownExpBottleMetadata::wrapperWrite(MetadataBuffer& wrapper) {
+    EntityMetadata::wrapperWrite(wrapper);
+    wrapper.writeItem(8, item);
+}
