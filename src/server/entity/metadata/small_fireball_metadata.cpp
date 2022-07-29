af739164f01f7d8edfec5757ec5e4134599fa4f8
JNNGL <jnngles@gmail.com>
2022-07-29 11:48:35 +1100

    Small fireball metadata

---
+++ src/server/entity/metadata/small_fireball_metadata.cpp
@@ -0,0 +1,9 @@
+#include "small_fireball_metadata.h"
+
+SmallFireballMetadata::SmallFireballMetadata() = default;
+SmallFireballMetadata::~SmallFireballMetadata() = default;
+
+void SmallFireballMetadata::wrapperWrite(MetadataBuffer& wrapper) {
+    EntityMetadata::wrapperWrite(wrapper);
+    wrapper.writeItem(8, item);
+}
