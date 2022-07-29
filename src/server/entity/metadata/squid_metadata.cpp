cfab476e9c37eaf6846bd3c7281840698850522f
JNNGL <jnngles@gmail.com>
2022-07-29 15:42:28 +1100

    Squid metadata

--- src/server/entity/metadata/squid_metadata.cpp
+++ src/server/entity/metadata/squid_metadata.cpp
@@ -1,1 +1,8 @@
 #include "squid_metadata.h"
+
+SquidMetadata::SquidMetadata() = default;
+SquidMetadata::~SquidMetadata() = default;
+
+void SquidMetadata::wrapperWrite(MetadataBuffer& wrapper) {
+    WaterAnimalMetadata::wrapperWrite(wrapper);
+}
--- src/server/entity/metadata/squid_metadata.h
+++ src/server/entity/metadata/squid_metadata.h
@@ -1,1 +1,13 @@
 #pragma once
+
+#include "water_animal_metadata.h"
+
+class SquidMetadata : public WaterAnimalMetadata {
+public:
+    SquidMetadata();
+    ~SquidMetadata() override;
+
+public:
+    void wrapperWrite(MetadataBuffer&) override;
+
+};
