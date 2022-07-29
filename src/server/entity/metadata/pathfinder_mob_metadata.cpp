4efee09a2fd0c5201a50e8590f56f99131766d0b
JNNGL <jnngles@gmail.com>
2022-07-29 15:39:15 +1100

    Pathfinder animal metadata

--- src/server/entity/metadata/pathfinder_mob_metadata.cpp
+++ src/server/entity/metadata/pathfinder_mob_metadata.cpp
@@ -1,1 +1,8 @@
 #include "pathfinder_mob_metadata.h"
+
+PathfinderMobMetadata::PathfinderMobMetadata() = default;
+PathfinderMobMetadata::~PathfinderMobMetadata() = default;
+
+void PathfinderMobMetadata::wrapperWrite(MetadataBuffer& wrapper) {
+    MobMetadata::wrapperWrite(wrapper);
+}
--- src/server/entity/metadata/pathfinder_mob_metadata.h
+++ src/server/entity/metadata/pathfinder_mob_metadata.h
@@ -1,1 +1,13 @@
 #pragma once
+
+#include "mob_metadata.h"
+
+class PathfinderMobMetadata : public MobMetadata {
+public:
+    PathfinderMobMetadata();
+    ~PathfinderMobMetadata() override;
+
+public:
+    void wrapperWrite(MetadataBuffer&) override;
+
+};
