fe18401ef1539e3f5eda1e8ecd1118211182774d
JNNGL <jnngles@gmail.com>
2022-07-29 15:19:10 +1100

    Armor stand metadata

--- src/server/entity/metadata/armor_stand_metadata.cpp
+++ src/server/entity/metadata/armor_stand_metadata.cpp
@@ -1,1 +1,15 @@
 #include "armor_stand_metadata.h"
+
+ArmorStandMetadata::ArmorStandMetadata() = default;
+ArmorStandMetadata::~ArmorStandMetadata() = default;
+
+void ArmorStandMetadata::wrapperWrite(MetadataBuffer& wrapper) {
+    LivingEntityMetadata::wrapperWrite(wrapper);
+    wrapper.writeByte(15, isMarker << 3 | hasNoBasePlate << 2 | hasArms << 1 | isSmall);
+    wrapper.writeRotation(16, headRotation.x, headRotation.y, headRotation.z);
+    wrapper.writeRotation(17, bodyRotation.x, bodyRotation.y, bodyRotation.z);
+    wrapper.writeRotation(18, leftArmRotation.x, leftArmRotation.y, leftArmRotation.z);
+    wrapper.writeRotation(19, rightArmRotation.x, rightArmRotation.y, rightArmRotation.z);
+    wrapper.writeRotation(20, leftLegRotation.x, leftLegRotation.y, leftLegRotation.z);
+    wrapper.writeRotation(21, rightLegRotation.x, rightLegRotation.y, rightLegRotation.z);
+}
--- src/server/entity/metadata/armor_stand_metadata.h
+++ src/server/entity/metadata/armor_stand_metadata.h
@@ -1,1 +1,25 @@
 #pragma once
+
+#include "living_entity_metadata.h"
+
+class ArmorStandMetadata : public LivingEntityMetadata {
+public:
+    ArmorStandMetadata();
+    ~ArmorStandMetadata() override;
+
+public:
+    void wrapperWrite(MetadataBuffer &) override;
+
+public:
+    bool isSmall = false;
+    bool hasArms = false;
+    bool hasNoBasePlate = false;
+    bool isMarker = false;
+    Rotation3f headRotation;
+    Rotation3f bodyRotation;
+    Rotation3f leftArmRotation;
+    Rotation3f rightArmRotation;
+    Rotation3f leftLegRotation;
+    Rotation3f rightLegRotation;
+
+};
