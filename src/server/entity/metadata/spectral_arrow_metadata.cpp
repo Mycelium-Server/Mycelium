06710a8a71838fbe2d4ecf100902f8063bbe0391
JNNGL <jnngles@gmail.com>
2022-07-29 11:27:23 +1100

    Spectral arrow metadata

---
+++ src/server/entity/metadata/spectral_arrow_metadata.cpp
@@ -0,0 +1,4 @@
+#include "spectral_arrow_metadata.h"
+
+SpectralArrowMetadata::SpectralArrowMetadata() = default;
+SpectralArrowMetadata::~SpectralArrowMetadata() = default;
---
+++ src/server/entity/metadata/spectral_arrow_metadata.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "abstract_arrow_metadata.h"
+
+class SpectralArrowMetadata : public AbstractArrowMetadata {
+public:
+    SpectralArrowMetadata();
+    ~SpectralArrowMetadata() override;
+
+};
