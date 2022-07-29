a615c7d3a36faef25a926f9d7c6acf923158d3a8
JNNGL <jnngles@gmail.com>
2022-07-29 11:51:59 +1100

    Fireball metadata

---
+++ src/server/entity/metadata/fireball_metadata.cpp
@@ -0,0 +1,9 @@
+#include "fireball_metadata.h"
+
+FireballMetadata::FireballMetadata() = default;
+FireballMetadata::~FireballMetadata() = default;
+
+void FireballMetadata::wrapperWrite(MetadataBuffer& wrapper) {
+    EntityMetadata::wrapperWrite(wrapper);
+    wrapper.writeItem(8, item);
+}
