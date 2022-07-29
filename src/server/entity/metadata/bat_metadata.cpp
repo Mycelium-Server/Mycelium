f4b06ccddfcf986674c4dae796b4c4bf22c8aeab
JNNGL <jnngles@gmail.com>
2022-07-29 15:34:58 +1100

    Bat metadata

--- src/server/entity/metadata/bat_metadata.cpp
+++ src/server/entity/metadata/bat_metadata.cpp
@@ -1,1 +1,9 @@
 #include "bat_metadata.h"
+
+BatMetadata::BatMetadata() = default;
+BatMetadata::~BatMetadata() = default;
+
+void BatMetadata::wrapperWrite(MetadataBuffer& wrapper) {
+    AmbientCreatureMetadata::wrapperWrite(wrapper);
+    wrapper.writeByte(16, isHanging);
+}
--- src/server/entity/metadata/bat_metadata.h
+++ src/server/entity/metadata/bat_metadata.h
@@ -1,1 +1,16 @@
 #pragma once
+
+#include "ambient_creature_metadata.h"
+
+class BatMetadata : public AmbientCreatureMetadata {
+public:
+    BatMetadata();
+    ~BatMetadata() override;
+
+public:
+    void wrapperWrite(MetadataBuffer&) override;
+
+public:
+    bool isHanging = false;
+
+};
