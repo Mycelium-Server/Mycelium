0803de92198969048e1c6b6b1e0eb6bbfe9b9f14
JNNGL <jnngles@gmail.com>
2022-07-29 16:37:02 +1100

    Chested horse metadata

--- src/server/entity/metadata/chested_horse_metadata.cpp
+++ src/server/entity/metadata/chested_horse_metadata.cpp
@@ -1,1 +1,9 @@
 #include "chested_horse_metadata.h"
+
+ChestedHorseMetadata::ChestedHorseMetadata() = default;
+ChestedHorseMetadata::~ChestedHorseMetadata() = default;
+
+void ChestedHorseMetadata::wrapperWrite(MetadataBuffer& wrapper) {
+    AbstractHorseMetadata::wrapperWrite(wrapper);
+    wrapper.writeBool(19, hasChest);
+}
