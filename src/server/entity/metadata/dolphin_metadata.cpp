f5f6bbf7865384a2686bd69efd713abcecc29354
JNNGL <jnngles@gmail.com>
2022-07-29 15:47:13 +1100

    Dolphin metadata

--- src/server/entity/metadata/dolphin_metadata.cpp
+++ src/server/entity/metadata/dolphin_metadata.cpp
@@ -1,1 +1,11 @@
 #include "dolphin_metadata.h"
+
+DolphinMetadata::DolphinMetadata() = default;
+DolphinMetadata::~DolphinMetadata() = default;
+
+void DolphinMetadata::wrapperWrite(MetadataBuffer& wrapper) {
+    WaterAnimalMetadata::wrapperWrite(wrapper);
+    wrapper.writePosition(16, treasurePosition);
+    wrapper.writeBool(17, hasFish);
+    wrapper.writeVarInt(18, moistureLevel);
+}
--- src/server/entity/metadata/dolphin_metadata.h
+++ src/server/entity/metadata/dolphin_metadata.h
@@ -1,1 +1,18 @@
 #pragma once
+
+#include "water_animal_metadata.h"
+
+class DolphinMetadata : public WaterAnimalMetadata {
+public:
+    DolphinMetadata();
+    ~DolphinMetadata() override;
+
+public:
+    void wrapperWrite(MetadataBuffer&) override;
+
+public:
+    ProtocolPosition treasurePosition {};
+    bool hasFish = false;
+    int moistureLevel = 2400;
+
+};
