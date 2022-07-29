5e69f9bea50819613c0e9659921ce30a2a847884
JNNGL <jnngles@gmail.com>
2022-07-29 16:50:47 +1100

    Bee metadata

--- src/server/entity/metadata/bee_metadata.cpp
+++ src/server/entity/metadata/bee_metadata.cpp
@@ -1,1 +1,9 @@
 #include "bee_metadata.h"
+
+BeeMetadata::BeeMetadata() = default;
+BeeMetadata::~BeeMetadata() = default;
+
+void BeeMetadata::wrapperWrite(MetadataBuffer& wrapper) {
+    AnimalMetadata::wrapperWrite(wrapper);
+    wrapper.writeByte(17, hasNectar << 3 | hasStung << 2 | isAngry << 1);
+}
--- src/server/entity/metadata/bee_metadata.h
+++ src/server/entity/metadata/bee_metadata.h
@@ -1,1 +1,19 @@
 #pragma once
+
+#include "animal_metadata.h"
+
+class BeeMetadata : public AnimalMetadata {
+public:
+    BeeMetadata();
+    ~BeeMetadata() override;
+
+public:
+    void wrapperWrite(MetadataBuffer&) override;
+
+public:
+    bool isAngry = false;
+    bool hasStung = false;
+    bool hasNectar = false;
+    int angerTime = 0;
+
+};
