1b4bc29e52210102f68c52d89b9d82a8b8bff99a
JNNGL <jnngles@gmail.com>
2022-07-29 16:44:08 +1100

    Trader llama metadata

--- src/server/entity/metadata/trader_llama_metadata.cpp
+++ src/server/entity/metadata/trader_llama_metadata.cpp
@@ -1,1 +1,5 @@
 #include "trader_llama_metadata.h"
+
+TraderLlamaMetadata::TraderLlamaMetadata() = default;
+TraderLlamaMetadata::~TraderLlamaMetadata() = default;
+void TraderLlamaMetadata::wrapperWrite(MetadataBuffer&) {}
--- src/server/entity/metadata/trader_llama_metadata.h
+++ src/server/entity/metadata/trader_llama_metadata.h
@@ -1,1 +1,13 @@
 #pragma once
+
+#include "metadata.h"
+
+class TraderLlamaMetadata : public AbstractEntityMetadata {
+public:
+    TraderLlamaMetadata();
+    ~TraderLlamaMetadata() override;
+
+public:
+    void wrapperWrite(MetadataBuffer&) override;
+
+};
