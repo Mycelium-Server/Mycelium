b0b4aed895c3225f79d3dbba35f600ff4b29e1f2
JNNGL <jnngles@gmail.com>
2022-07-29 16:41:44 +1100

    Llama metadata

--- src/server/entity/metadata/llama_metadata.h
+++ src/server/entity/metadata/llama_metadata.h
@@ -1,1 +1,18 @@
 #pragma once
+
+#include "chested_horse_metadata.h"
+
+class LlamaMetadata : public ChestedHorseMetadata {
+public:
+    LlamaMetadata();
+    ~LlamaMetadata() override;
+
+public:
+    void wrapperWrite(MetadataBuffer&) override;
+
+public:
+    int strength = 0;
+    int carpetColor = -1;
+    int variant = 0;
+
+};
