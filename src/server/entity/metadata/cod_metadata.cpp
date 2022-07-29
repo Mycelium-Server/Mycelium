9904802b16baa5cbc2375843aa3f77a2c4b499e5
JNNGL <jnngles@gmail.com>
2022-07-29 15:49:51 +1100

    cod metadata

--- src/server/entity/metadata/cod_metadata.cpp
+++ src/server/entity/metadata/cod_metadata.cpp
@@ -1,1 +1,8 @@
 #include "cod_metadata.h"
+
+CodMetadata::CodMetadata() = default;
+CodMetadata::~CodMetadata() = default;
+
+void CodMetadata::wrapperWrite(MetadataBuffer& wrapper) {
+    AbstractFishMetadata::wrapperWrite(wrapper);
+}
--- src/server/entity/metadata/cod_metadata.h
+++ src/server/entity/metadata/cod_metadata.h
@@ -1,1 +1,13 @@
 #pragma once
+
+#include "abstract_fish_metadata.h"
+
+class CodMetadata : public AbstractFishMetadata {
+public:
+    CodMetadata();
+    ~CodMetadata() override;
+
+public:
+    void wrapperWrite(MetadataBuffer&) override;
+
+};
