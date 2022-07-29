b016ad35b6f14efc21780df86d41bdb3be0b7bfe
JNNGL <jnngles@gmail.com>
2022-07-29 16:35:33 +1100

    Skeleton horse metadata

--- src/server/entity/metadata/skeleton_horse_metadata.cpp
+++ src/server/entity/metadata/skeleton_horse_metadata.cpp
@@ -1,1 +1,8 @@
 #include "skeleton_horse_metadata.h"
+
+SkeletonHorseMetadata::SkeletonHorseMetadata() = default;
+SkeletonHorseMetadata::~SkeletonHorseMetadata() = default;
+
+void SkeletonHorseMetadata::wrapperWrite(MetadataBuffer& wrapper) {
+    AbstractHorseMetadata::wrapperWrite(wrapper);
+}
--- src/server/entity/metadata/skeleton_horse_metadata.h
+++ src/server/entity/metadata/skeleton_horse_metadata.h
@@ -1,1 +1,13 @@
 #pragma once
+
+#include "abstract_horse_metadata.h"
+
+class SkeletonHorseMetadata : public AbstractHorseMetadata {
+public:
+    SkeletonHorseMetadata();
+    ~SkeletonHorseMetadata() override;
+
+public:
+    void wrapperWrite(MetadataBuffer&) override;
+
+};
