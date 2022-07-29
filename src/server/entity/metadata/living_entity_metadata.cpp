e5dc98973d18f5d292ad2c4ac6e1ed2bf8b337f0
JNNGL <jnngles@gmail.com>
2022-07-29 14:57:32 +1100

    Living entity metadata

--- src/server/entity/metadata/living_entity_metadata.cpp
+++ src/server/entity/metadata/living_entity_metadata.cpp
@@ -1,1 +1,15 @@
 #include "living_entity_metadata.h"
+
+LivingEntityMetadata::LivingEntityMetadata() = default;
+LivingEntityMetadata::~LivingEntityMetadata() = default;
+
+void LivingEntityMetadata::wrapperWrite(MetadataBuffer& wrapper) {
+    EntityMetadata::wrapperWrite(wrapper);
+    wrapper.writeByte(8, inRiptideSpinAttack << 3 | offhand << 2 | isHandActive);
+    wrapper.writeFloat(9, health);
+    wrapper.writeVarInt(10, potionEffectColor);
+    wrapper.writeBool(11, ambientPotionEffect);
+    wrapper.writeVarInt(12, numArrows);
+    wrapper.writeVarInt(13, numBeeStingers);
+    wrapper.writeOptPosition(14, bedLocation);
+}
--- src/server/entity/metadata/living_entity_metadata.h
+++ src/server/entity/metadata/living_entity_metadata.h
@@ -1,1 +1,24 @@
 #pragma once
+
+#include "entity_metadata.h"
+
+class LivingEntityMetadata : public EntityMetadata {
+public:
+    LivingEntityMetadata();
+    ~LivingEntityMetadata() override;
+
+public:
+    void wrapperWrite(MetadataBuffer&) override;
+
+public:
+    bool isHandActive = false;
+    bool offhand = false;
+    bool inRiptideSpinAttack = false;
+    float health = 1.0;
+    int potionEffectColor = 0;
+    bool ambientPotionEffect = false;
+    int numArrows = 0;
+    int numBeeStingers = 0;
+    std::optional<ProtocolPosition> bedLocation = {};
+
+};
