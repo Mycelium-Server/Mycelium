bc6f94c5f8e4f59f23709b760e1380196e0b569e
JNNGL <jnngles@gmail.com>
2022-07-29 13:27:01 +1100

    Item entity metadata

--- src/server/entity/metadata/item_entity_metadata.cpp
+++ src/server/entity/metadata/item_entity_metadata.cpp
@@ -1,1 +1,9 @@
 #include "item_entity_metadata.h"
+
+ItemEntityMetadata::ItemEntityMetadata() = default;
+ItemEntityMetadata::~ItemEntityMetadata() = default;
+
+void ItemEntityMetadata::wrapperWrite(MetadataBuffer& wrapper) {
+    EntityMetadata::wrapperWrite(wrapper);
+    wrapper.writeItem(8, item);
+}
--- src/server/entity/metadata/item_entity_metadata.h
+++ src/server/entity/metadata/item_entity_metadata.h
@@ -1,1 +1,16 @@
 #pragma once
+
+#include "entity_metadata.h"
+
+class ItemEntityMetadata : public EntityMetadata {
+public:
+    ItemEntityMetadata();
+    ~ItemEntityMetadata() override;
+
+public:
+    void wrapperWrite(MetadataBuffer&) override;
+
+public:
+    ItemStack item;
+
+};
