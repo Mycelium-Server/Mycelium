4e5f54f82c34a69552c89645427a363ac3005c1b
JNNGL <jnngles@gmail.com>
2022-07-29 16:05:10 +1100

    Horse metadata

--- src/server/entity/metadata/horse_metadata.cpp
+++ src/server/entity/metadata/horse_metadata.cpp
@@ -1,1 +1,9 @@
 #include "horse_metadata.h"
+
+HorseMetadata::HorseMetadata() = default;
+HorseMetadata::~HorseMetadata() = default;
+
+void HorseMetadata::wrapperWrite(MetadataBuffer& wrapper) {
+    AbstractHorseMetadata::wrapperWrite(wrapper);
+    wrapper.writeVarInt(19, variant);
+}
--- src/server/entity/metadata/horse_metadata.h
+++ src/server/entity/metadata/horse_metadata.h
@@ -1,1 +1,16 @@
 #pragma once
+
+#include "abstract_horse_metadata.h"
+
+class HorseMetadata : public AbstractHorseMetadata {
+public:
+    HorseMetadata();
+    ~HorseMetadata() override;
+
+public:
+    void wrapperWrite(MetadataBuffer &) override;
+
+public:
+    int variant = 0;
+
+};
