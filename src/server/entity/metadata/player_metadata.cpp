7d7627f1b1e680751b34c30ee32eae1a9daa9cce
JNNGL <jnngles@gmail.com>
2022-07-29 15:06:29 +1100

    Player metadata

--- src/server/entity/metadata/player_metadata.cpp
+++ src/server/entity/metadata/player_metadata.cpp
@@ -1,1 +1,14 @@
 #include "player_metadata.h"
+
+PlayerMetadata::PlayerMetadata() = default;
+PlayerMetadata::~PlayerMetadata() = default;
+
+void PlayerMetadata::wrapperWrite(MetadataBuffer& wrapper) {
+    EntityMetadata::wrapperWrite(wrapper);
+    wrapper.writeFloat(15, additionalHearts);
+    wrapper.writeVarInt(16, score);
+    wrapper.writeByte(17, displayedSkinParts.value);
+    wrapper.writeByte(18, (unsigned char) mainHand);
+    wrapper.writeNBT(19, leftShoulderEntityData);
+    wrapper.writeNBT(20, rightShoulderEntityData);
+}
--- src/server/entity/metadata/player_metadata.h
+++ src/server/entity/metadata/player_metadata.h
@@ -1,1 +1,22 @@
 #pragma once
+
+#include "entity_metadata.h"
+#include "../../client_settings.h"
+
+class PlayerMetadata : public EntityMetadata {
+public:
+    PlayerMetadata();
+    ~PlayerMetadata() override;
+
+public:
+    void wrapperWrite(MetadataBuffer&) override;
+
+public:
+    float additionalHearts = 0;
+    int score = 0;
+    SkinParts displayedSkinParts { .value = 0 };
+    MainHand mainHand = MainHand::RIGHT;
+    std::shared_ptr<NBT_Component> leftShoulderEntityData;
+    std::shared_ptr<NBT_Component> rightShoulderEntityData;
+
+};
