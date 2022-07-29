871cc14fba92c24c7f5ef77daa86987085e4cbee
JNNGL <jnngles@gmail.com>
2022-07-29 15:48:44 +1100

    Abstract fish metadata

--- src/server/entity/metadata/abstract_fish_metadata.cpp
+++ src/server/entity/metadata/abstract_fish_metadata.cpp
@@ -1,1 +1,9 @@
 #include "abstract_fish_metadata.h"
+
+AbstractFishMetadata::AbstractFishMetadata() = default;
+AbstractFishMetadata::~AbstractFishMetadata() = default;
+
+void AbstractFishMetadata::wrapperWrite(MetadataBuffer& wrapper) {
+    WaterAnimalMetadata::wrapperWrite(wrapper);
+    wrapper.writeBool(16, fromBucket);
+}
--- src/server/entity/metadata/abstract_fish_metadata.h
+++ src/server/entity/metadata/abstract_fish_metadata.h
@@ -1,1 +1,16 @@
 #pragma once
+
+#include "water_animal_metadata.h"
+
+class AbstractFishMetadata : public WaterAnimalMetadata {
+public:
+    AbstractFishMetadata();
+    ~AbstractFishMetadata() override;
+
+public:
+    void wrapperWrite(MetadataBuffer&) override;
+
+public:
+    bool fromBucket = false;
+
+};
