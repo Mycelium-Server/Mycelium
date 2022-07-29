d02e088020af41a83d8b898069a7470ce1f919a9
JNNGL <jnngles@gmail.com>
2022-07-29 15:55:01 +1100

    Tropical fish metadata

--- src/server/entity/metadata/tropical_fish_metadata.cpp
+++ src/server/entity/metadata/tropical_fish_metadata.cpp
@@ -1,1 +1,9 @@
 #include "tropical_fish_metadata.h"
+
+TropicalFishMetadata::TropicalFishMetadata() = default;
+TropicalFishMetadata::~TropicalFishMetadata() = default;
+
+void TropicalFishMetadata::wrapperWrite(MetadataBuffer& wrapper) {
+    AbstractFishMetadata::wrapperWrite(wrapper);
+    wrapper.writeVarInt(17, variant);
+}
--- src/server/entity/metadata/tropical_fish_metadata.h
+++ src/server/entity/metadata/tropical_fish_metadata.h
@@ -1,1 +1,16 @@
 #pragma once
+
+#include "abstract_fish_metadata.h"
+
+class TropicalFishMetadata : public AbstractFishMetadata {
+public:
+    TropicalFishMetadata();
+    ~TropicalFishMetadata() override;
+
+public:
+    void wrapperWrite(MetadataBuffer&) override;
+
+public:
+    int variant = 0;
+
+};
