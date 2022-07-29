d49aff46bf561d88690a0d0717ff024c96556034
JNNGL <jnngles@gmail.com>
2022-07-29 16:53:27 +1100

    Fix bee metadata

--- src/server/entity/metadata/bee_metadata.cpp
+++ src/server/entity/metadata/bee_metadata.cpp
@@ -6,4 +6,5 @@ BeeMetadata::~BeeMetadata() = default;
 void BeeMetadata::wrapperWrite(MetadataBuffer& wrapper) {
     AnimalMetadata::wrapperWrite(wrapper);
     wrapper.writeByte(17, hasNectar << 3 | hasStung << 2 | isAngry << 1);
+    wrapper.writeVarInt(18, angerTime);
 }
