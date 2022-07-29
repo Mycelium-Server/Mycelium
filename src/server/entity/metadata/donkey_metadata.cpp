72a6a52a5d2e221e1099ac49e16e46b77bfb8365
JNNGL <jnngles@gmail.com>
2022-07-29 16:38:31 +1100

    Donkey metadata

--- src/server/entity/metadata/donkey_metadata.cpp
+++ src/server/entity/metadata/donkey_metadata.cpp
@@ -1,1 +1,8 @@
 #include "donkey_metadata.h"
+
+DonkeyMetadata::DonkeyMetadata() = default;
+DonkeyMetadata::~DonkeyMetadata() = default;
+
+void DonkeyMetadata::wrapperWrite(MetadataBuffer& wrapper) {
+    ChestedHorseMetadata::wrapperWrite(wrapper);
+}
--- src/server/entity/metadata/donkey_metadata.h
+++ src/server/entity/metadata/donkey_metadata.h
@@ -1,1 +1,13 @@
 #pragma once
+
+#include "chested_horse_metadata.h"
+
+class DonkeyMetadata : public ChestedHorseMetadata {
+public:
+    DonkeyMetadata();
+    ~DonkeyMetadata() override;
+
+public:
+    void wrapperWrite(MetadataBuffer&) override;
+
+};
