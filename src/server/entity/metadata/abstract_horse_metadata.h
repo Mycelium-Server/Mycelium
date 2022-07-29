a3f45d345ad790e14ff4fcdbdaa887015b820ced
JNNGL <jnngles@gmail.com>
2022-07-29 16:03:33 +1100

    Abstract horse metadata

--- src/server/entity/metadata/abstract_horse_metadata.h
+++ src/server/entity/metadata/abstract_horse_metadata.h
@@ -1,1 +1,22 @@
 #pragma once
+
+#include "animal_metadata.h"
+
+class AbstractHorseMetadata : public AnimalMetadata {
+public:
+    AbstractHorseMetadata();
+    ~AbstractHorseMetadata() override;
+
+public:
+    void wrapperWrite(MetadataBuffer&) override;
+
+public:
+    bool isTame = false;
+    bool isSaddled = false;
+    bool hasBred = false;
+    bool isEating = false;
+    bool isRearing = false;
+    bool isMouthOpen = false;
+    std::optional<uuids::uuid> owner;
+
+};
