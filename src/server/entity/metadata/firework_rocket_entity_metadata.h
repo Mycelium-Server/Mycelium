1a0c4199ffabfd0e4536996a5dc80c99dc2a3abe
JNNGL <jnngles@gmail.com>
2022-07-29 12:01:57 +1100

    Firework rocket entity metadata

---
+++ src/server/entity/metadata/firework_rocket_entity_metadata.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "entity_metadata.h"
+#include "../entity.h"
+
+class FireworkRocketEntityMetadata : public EntityMetadata {
+public:
+    FireworkRocketEntityMetadata();
+    ~FireworkRocketEntityMetadata() override;
+
+public:
+    void wrapperWrite(MetadataBuffer&) override;
+
+public:
+    ItemStack fireworkInfo;
+    std::optional<Entity*> entity;
+    bool shotFromCrossbow = false;
+
+};
