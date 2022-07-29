d2d615876a7109f66c58b97664b14444faa4e0c5
JNNGL <jnngles@gmail.com>
2022-07-29 16:48:06 +1100

    Axolotl metadata

--- src/server/entity/metadata/axolotl_metadata.cpp
+++ src/server/entity/metadata/axolotl_metadata.cpp
@@ -1,1 +1,11 @@
 #include "axolotl_metadata.h"
+
+AxolotlMetadata::AxolotlMetadata() = default;
+AxolotlMetadata::~AxolotlMetadata() = default;
+
+void AxolotlMetadata::wrapperWrite(MetadataBuffer& wrapper) {
+    AnimalMetadata::wrapperWrite(wrapper);
+    wrapper.writeVarInt(17, variant);
+    wrapper.writeBool(18, playingDead);
+    wrapper.writeBool(19, fromBucket);
+}
--- src/server/entity/metadata/axolotl_metadata.h
+++ src/server/entity/metadata/axolotl_metadata.h
@@ -1,1 +1,18 @@
 #pragma once
+
+#include "animal_metadata.h"
+
+class AxolotlMetadata : public AnimalMetadata {
+public:
+    AxolotlMetadata();
+    ~AxolotlMetadata() override;
+
+public:
+    void wrapperWrite(MetadataBuffer &) override;
+
+public:
+    int variant = 0;
+    bool playingDead = false;
+    bool fromBucket = false;
+
+};
