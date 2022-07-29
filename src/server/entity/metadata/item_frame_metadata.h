dc82656522c79cf9a28bb2116256970bfa4f995e
JNNGL <jnngles@gmail.com>
2022-07-29 12:08:08 +1100

    Item frame metadata

---
+++ src/server/entity/metadata/item_frame_metadata.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "entity_metadata.h"
+
+class ItemFrameMetadata : public EntityMetadata {
+public:
+    ItemFrameMetadata();
+    ~ItemFrameMetadata() override;
+
+public:
+    void wrapperWrite(MetadataBuffer&) override;
+
+public:
+    ItemStack item;
+    int rotation = 0;
+
+};
