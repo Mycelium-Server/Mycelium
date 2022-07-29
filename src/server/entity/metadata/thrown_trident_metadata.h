b81485da62b0f41f222c423beae046f65b43be6c
JNNGL <jnngles@gmail.com>
2022-07-29 11:30:10 +1100

    Thrown trident metadata

---
+++ src/server/entity/metadata/thrown_trident_metadata.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "abstract_arrow_metadata.h"
+
+class ThrownTridentMetadata : public AbstractArrowMetadata {
+public:
+    ThrownTridentMetadata();
+    ~ThrownTridentMetadata() override;
+
+public:
+    void wrapperWrite(MetadataBuffer&) override;
+
+public:
+    int loyaltyLevel = 0;
+    bool hasEnchantmentGlint = false;
+
+};
