d11951d99fc68409d8c64030310765d1c0b1b28a
JNNGL <jnngles@gmail.com>
2022-07-29 15:57:32 +1100

    Ageable mob metadata

--- src/server/entity/metadata/ageable_mob_metadata.cpp
+++ src/server/entity/metadata/ageable_mob_metadata.cpp
@@ -1,1 +1,9 @@
 #include "ageable_mob_metadata.h"
+
+AgeableMobMetadata::AgeableMobMetadata() = default;
+AgeableMobMetadata::~AgeableMobMetadata() = default;
+
+void AgeableMobMetadata::wrapperWrite(MetadataBuffer& wrapper) {
+    PathfinderMobMetadata::wrapperWrite(wrapper);
+    wrapper.writeBool(16, isBaby);
+}
