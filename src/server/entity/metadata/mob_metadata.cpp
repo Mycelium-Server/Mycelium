cd2066af6ba2ec3a2e222c8de489cb1c822761ee
JNNGL <jnngles@gmail.com>
2022-07-29 15:22:13 +1100

    Mob metadata

--- src/server/entity/metadata/mob_metadata.cpp
+++ src/server/entity/metadata/mob_metadata.cpp
@@ -1,1 +1,9 @@
 #include "mob_metadata.h"
+
+MobMetadata::MobMetadata() = default;
+MobMetadata::~MobMetadata() = default;
+
+void MobMetadata::wrapperWrite(MetadataBuffer& wrapper) {
+    LivingEntityMetadata::wrapperWrite(wrapper);
+    wrapper.writeByte(15, isAggressive << 2 | isLeftHanded << 1 | noAI);
+}
--- src/server/entity/metadata/mob_metadata.h
+++ src/server/entity/metadata/mob_metadata.h
@@ -1,1 +1,18 @@
 #pragma once
+
+#include "living_entity_metadata.h"
+
+class MobMetadata : public LivingEntityMetadata {
+public:
+    MobMetadata();
+    ~MobMetadata() override;
+
+public:
+    void wrapperWrite(MetadataBuffer&) override;
+
+public:
+    bool noAI = false;
+    bool isLeftHanded = false;
+    bool isAggressive = false;
+
+};
