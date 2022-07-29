28d909b7a2c2c05fa5f1ed75fe0182ddf6450689
JNNGL <jnngles@gmail.com>
2022-07-29 15:39:24 +1100

    Water animal metadata

--- src/server/entity/metadata/water_animal_metadata.cpp
+++ src/server/entity/metadata/water_animal_metadata.cpp
@@ -1,1 +1,8 @@
 #include "water_animal_metadata.h"
+
+WaterAnimalMetadata::WaterAnimalMetadata() = default;
+WaterAnimalMetadata::~WaterAnimalMetadata() = default;
+
+void WaterAnimalMetadata::wrapperWrite(MetadataBuffer& wrapper) {
+    PathfinderMobMetadata::wrapperWrite(wrapper);
+}
--- src/server/entity/metadata/water_animal_metadata.h
+++ src/server/entity/metadata/water_animal_metadata.h
@@ -1,1 +1,13 @@
 #pragma once
+
+#include "pathfinder_mob_metadata.h"
+
+class WaterAnimalMetadata : public PathfinderMobMetadata {
+public:
+    WaterAnimalMetadata();
+    ~WaterAnimalMetadata() override;
+
+public:
+    void wrapperWrite(MetadataBuffer&) override;
+
+};
