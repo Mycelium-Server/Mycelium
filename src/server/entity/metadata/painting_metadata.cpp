e99686548e608dbc5818a2a88c38fc4dd78afb14
JNNGL <jnngles@gmail.com>
2022-07-29 14:34:01 +1100

    Painting metadata

--- src/server/entity/metadata/painting_metadata.cpp
+++ src/server/entity/metadata/painting_metadata.cpp
@@ -1,1 +1,9 @@
 #include "painting_metadata.h"
+
+PaintingMetadata::PaintingMetadata() = default;
+PaintingMetadata::~PaintingMetadata() = default;
+
+void PaintingMetadata::wrapperWrite(MetadataBuffer& wrapper) {
+    EntityMetadata::wrapperWrite(wrapper);
+    wrapper.writePaintingVariant(8, paintingType);
+}
--- src/server/entity/metadata/painting_metadata.h
+++ src/server/entity/metadata/painting_metadata.h
@@ -1,1 +1,17 @@
 #pragma once
+
+#include "entity_metadata.h"
+#include "painting_variant.h"
+
+class PaintingMetadata : public EntityMetadata {
+public:
+    PaintingMetadata();
+    ~PaintingMetadata() override;
+
+public:
+    void wrapperWrite(MetadataBuffer&) override;
+
+public:
+    PaintingVariant paintingType = PAINTING_KEBAB;
+
+};
