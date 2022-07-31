bc93f349adda05530b5b5983dfbc22cd2b52bf58
JNNGL <jnngles@gmail.com>
2022-07-31 13:29:49 +1100

    Elder guardian metadata

--- src/server/entity/metadata/elder_guardian_metadata.cpp
+++ src/server/entity/metadata/elder_guardian_metadata.cpp
@@ -1,1 +1,8 @@
 #include "elder_guardian_metadata.h"
+
+ElderGuardianMetadata::ElderGuardianMetadata() = default;
+ElderGuardianMetadata::~ElderGuardianMetadata() = default;
+
+void ElderGuardianMetadata::wrapperWrite(MetadataBuffer& wrapper) {
+    GuardianMetadata::wrapperWrite(wrapper);
+}
--- src/server/entity/metadata/elder_guardian_metadata.h
+++ src/server/entity/metadata/elder_guardian_metadata.h
@@ -1,1 +1,13 @@
 #pragma once
+
+#include "guardian_metadata.h"
+
+class ElderGuardianMetadata : public GuardianMetadata {
+public:
+    ElderGuardianMetadata();
+    ~ElderGuardianMetadata() override;
+
+public:
+    void wrapperWrite(MetadataBuffer&) override;
+
+};
