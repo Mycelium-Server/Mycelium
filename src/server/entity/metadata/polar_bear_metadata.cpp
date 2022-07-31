c9e7beefe4f6da101f997ac224ef6140361c4c11
JNNGL <jnngles@gmail.com>
2022-07-31 11:52:47 +1100

    Polar bear metadata

--- src/server/entity/metadata/polar_bear_metadata.cpp
+++ src/server/entity/metadata/polar_bear_metadata.cpp
@@ -1,1 +1,9 @@
 #include "polar_bear_metadata.h"
+
+PolarBearMetadata::PolarBearMetadata() = default;
+PolarBearMetadata::~PolarBearMetadata() = default;
+
+void PolarBearMetadata::wrapperWrite(MetadataBuffer& wrapper) {
+    AnimalMetadata::wrapperWrite(wrapper);
+    wrapper.writeBool(17, isStandingUp);
+}
--- src/server/entity/metadata/polar_bear_metadata.h
+++ src/server/entity/metadata/polar_bear_metadata.h
@@ -1,1 +1,16 @@
 #pragma once
+
+#include "animal_metadata.h"
+
+class PolarBearMetadata : public AnimalMetadata {
+public:
+    PolarBearMetadata();
+    ~PolarBearMetadata() override;
+
+public:
+    void wrapperWrite(MetadataBuffer&) override;
+
+public:
+    bool isStandingUp = false;
+
+};
