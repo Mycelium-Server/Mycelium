7fe97a643c99e5b72fad36e6f87780e931b76fb2
JNNGL <jnngles@gmail.com>
2022-07-31 12:13:25 +1100

    Tameable animal metadata

--- src/server/entity/metadata/tameable_animal_metadata.h
+++ src/server/entity/metadata/tameable_animal_metadata.h
@@ -1,1 +1,18 @@
 #pragma once
+
+#include "animal_metadata.h"
+
+class TameableAnimalMetadata : public AnimalMetadata {
+public:
+    TameableAnimalMetadata();
+    ~TameableAnimalMetadata() override;
+
+public:
+    void wrapperWrite(MetadataBuffer&) override;
+
+public:
+    bool isSitting = false;
+    bool isTamed = false;
+    std::optional<uuids::uuid> owner;
+
+};
