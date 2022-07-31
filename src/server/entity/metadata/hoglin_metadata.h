d468a6eb547bd6f7d1d3e4ae1137070108da2b74
JNNGL <jnngles@gmail.com>
2022-07-31 11:58:10 +1100

    Hoglin metadata

--- src/server/entity/metadata/hoglin_metadata.h
+++ src/server/entity/metadata/hoglin_metadata.h
@@ -1,1 +1,16 @@
 #pragma once
+
+#include "animal_metadata.h"
+
+class HoglinMetadata : public AnimalMetadata {
+public:
+    HoglinMetadata();
+    ~HoglinMetadata() override;
+
+public:
+    void wrapperWrite(MetadataBuffer&) override;
+
+public:
+    bool isImmuneToZombification = false;
+
+};
