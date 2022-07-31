9c49cfeed29e82781edaf278ff1053f798e8f12f
JNNGL <jnngles@gmail.com>
2022-07-31 13:23:51 +1100

    Endermite metadata

--- src/server/entity/metadata/endermite_metadata.cpp
+++ src/server/entity/metadata/endermite_metadata.cpp
@@ -1,1 +1,8 @@
 #include "endermite_metadata.h"
+
+EndermiteMetadata::EndermiteMetadata() = default;
+EndermiteMetadata::~EndermiteMetadata() = default;
+
+void EndermiteMetadata::wrapperWrite(MetadataBuffer& wrapper) {
+    MonsterMetadata::wrapperWrite(wrapper);
+}
--- src/server/entity/metadata/endermite_metadata.h
+++ src/server/entity/metadata/endermite_metadata.h
@@ -1,1 +1,13 @@
 #pragma once
+
+#include "monster_metadata.h"
+
+class EndermiteMetadata : public MonsterMetadata {
+public:
+    EndermiteMetadata();
+    ~EndermiteMetadata() override;
+
+public:
+    void wrapperWrite(MetadataBuffer&) override;
+
+};
