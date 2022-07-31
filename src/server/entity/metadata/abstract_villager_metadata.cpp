12b2b7c833fc2c6d62e8d962ace0002c7f72674b
JNNGL <jnngles@gmail.com>
2022-07-31 12:43:28 +1100

    Abstract villager metadata

--- src/server/entity/metadata/abstract_villager_metadata.cpp
+++ src/server/entity/metadata/abstract_villager_metadata.cpp
@@ -1,1 +1,9 @@
 #include "abstract_villager_metadata.h"
+
+AbstractVillagerMetadata::AbstractVillagerMetadata() = default;
+AbstractVillagerMetadata::~AbstractVillagerMetadata() = default;
+
+void AbstractVillagerMetadata::wrapperWrite(MetadataBuffer& wrapper) {
+    AgeableMobMetadata::wrapperWrite(wrapper);
+    wrapper.writeVarInt(17, headShakeTimer);
+}
--- src/server/entity/metadata/abstract_villager_metadata.h
+++ src/server/entity/metadata/abstract_villager_metadata.h
@@ -1,1 +1,16 @@
 #pragma once
+
+#include "ageable_mob_metadata.h"
+
+class AbstractVillagerMetadata : public AgeableMobMetadata {
+public:
+    AbstractVillagerMetadata();
+    ~AbstractVillagerMetadata() override;
+
+public:
+    void wrapperWrite(MetadataBuffer&) override;
+
+public:
+    int headShakeTimer = 0;
+
+};
