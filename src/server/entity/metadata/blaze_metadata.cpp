7cd96b8a8b219ac464147beaafeb77d974d97a4a
JNNGL <jnngles@gmail.com>
2022-07-31 13:20:44 +1100

    Blaze metadata

--- src/server/entity/metadata/blaze_metadata.cpp
+++ src/server/entity/metadata/blaze_metadata.cpp
@@ -1,1 +1,9 @@
 #include "blaze_metadata.h"
+
+BlazeMetadata::BlazeMetadata() = default;
+BlazeMetadata::~BlazeMetadata() = default;
+
+void BlazeMetadata::wrapperWrite(MetadataBuffer& wrapper) {
+    MonsterMetadata::wrapperWrite(wrapper);
+    wrapper.writeByte(16, isOnFire);
+}
--- src/server/entity/metadata/blaze_metadata.h
+++ src/server/entity/metadata/blaze_metadata.h
@@ -1,1 +1,16 @@
 #pragma once
+
+#include "monster_metadata.h"
+
+class BlazeMetadata : public MonsterMetadata {
+public:
+    BlazeMetadata();
+    ~BlazeMetadata() override;
+
+public:
+    void wrapperWrite(MetadataBuffer&) override;
+
+public:
+    bool isOnFire = false;
+
+};
