dc2d8c9c481fe5f8017e795a00baed3a3fa2848f
JNNGL <jnngles@gmail.com>
2022-07-31 12:46:57 +1100

    Villager metadata

--- src/server/entity/metadata/villager_metadata.cpp
+++ src/server/entity/metadata/villager_metadata.cpp
@@ -1,1 +1,9 @@
 #include "villager_metadata.h"
+
+VillagerMetadata::VillagerMetadata() = default;
+VillagerMetadata::~VillagerMetadata() = default;
+
+void VillagerMetadata::wrapperWrite(MetadataBuffer& wrapper) {
+    AbstractVillagerMetadata::wrapperWrite(wrapper);
+    wrapper.writeVillagerData(18, villagerData);
+}
--- src/server/entity/metadata/villager_metadata.h
+++ src/server/entity/metadata/villager_metadata.h
@@ -1,1 +1,16 @@
 #pragma once
+
+#include "abstract_villager_metadata.h"
+
+class VillagerMetadata : public AbstractVillagerMetadata {
+public:
+    VillagerMetadata();
+    ~VillagerMetadata() override;
+
+public:
+    void wrapperWrite(MetadataBuffer&) override;
+
+public:
+    VillagerData villagerData { MINECRAFT_PLAINS, MINECRAFT_NONE, 1 };
+
+};
