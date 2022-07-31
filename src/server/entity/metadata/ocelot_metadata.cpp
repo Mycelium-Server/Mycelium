6125b1074ac8bf264126bbbbb20c82fad4077f88
JNNGL <jnngles@gmail.com>
2022-07-31 11:32:43 +1100

    Ocelot metadata

--- src/server/entity/metadata/ocelot_metadata.cpp
+++ src/server/entity/metadata/ocelot_metadata.cpp
@@ -1,1 +1,9 @@
 #include "ocelot_metadata.h"
+
+OcelotMetadata::OcelotMetadata() = default;
+OcelotMetadata::~OcelotMetadata() = default;
+
+void OcelotMetadata::wrapperWrite(MetadataBuffer& wrapper) {
+    AnimalMetadata::wrapperWrite(wrapper);
+    wrapper.writeBool(17, isTrusting);
+}
--- src/server/entity/metadata/ocelot_metadata.h
+++ src/server/entity/metadata/ocelot_metadata.h
@@ -1,1 +1,16 @@
 #pragma once
+
+#include "animal_metadata.h"
+
+class OcelotMetadata : public AnimalMetadata {
+public:
+    OcelotMetadata();
+    ~OcelotMetadata() override;
+
+public:
+    void wrapperWrite(MetadataBuffer&) override;
+
+public:
+    bool isTrusting = false;
+
+};
