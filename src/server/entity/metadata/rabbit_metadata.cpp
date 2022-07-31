fae09dcd043eded7759fc5c3d8ef78a9708ea89c
JNNGL <jnngles@gmail.com>
2022-07-31 11:47:01 +1100

    Rabbit metadata

--- src/server/entity/metadata/rabbit_metadata.cpp
+++ src/server/entity/metadata/rabbit_metadata.cpp
@@ -1,1 +1,9 @@
 #include "rabbit_metadata.h"
+
+RabbitMetadata::RabbitMetadata() = default;
+RabbitMetadata::~RabbitMetadata() = default;
+
+void RabbitMetadata::wrapperWrite(MetadataBuffer& wrapper) {
+    AnimalMetadata::wrapperWrite(wrapper);
+    wrapper.writeVarInt(17, type);
+}
--- src/server/entity/metadata/rabbit_metadata.h
+++ src/server/entity/metadata/rabbit_metadata.h
@@ -1,1 +1,16 @@
 #pragma once
+
+#include "animal_metadata.h"
+
+class RabbitMetadata : public AnimalMetadata {
+public:
+    RabbitMetadata();
+    ~RabbitMetadata() override;
+
+public:
+    void wrapperWrite(MetadataBuffer&) override;
+
+public:
+    int type = 0;
+
+};
