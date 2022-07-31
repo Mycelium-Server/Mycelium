19874a1869d8c7a93dc2589d8368606ff1c0b0a3
JNNGL <jnngles@gmail.com>
2022-07-31 13:30:54 +1100

    Silverfish metadata

--- src/server/entity/metadata/silverfish_metadata.cpp
+++ src/server/entity/metadata/silverfish_metadata.cpp
@@ -1,1 +1,8 @@
 #include "silverfish_metadata.h"
+
+SilverfishMetadata::SilverfishMetadata() = default;
+SilverfishMetadata::~SilverfishMetadata() = default;
+
+void SilverfishMetadata::wrapperWrite(MetadataBuffer& wrapper) {
+    MonsterMetadata::wrapperWrite(wrapper);
+}
--- src/server/entity/metadata/silverfish_metadata.h
+++ src/server/entity/metadata/silverfish_metadata.h
@@ -1,1 +1,13 @@
 #pragma once
+
+#include "monster_metadata.h"
+
+class SilverfishMetadata : public MonsterMetadata {
+public:
+    SilverfishMetadata();
+    ~SilverfishMetadata() override;
+
+public:
+    void wrapperWrite(MetadataBuffer&) override;
+
+};
