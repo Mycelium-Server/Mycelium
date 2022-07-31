c4079c9b9698edd80df942ee37500f4b8a2f6210
JNNGL <jnngles@gmail.com>
2022-07-31 12:25:48 +1100

    Parrot metadata

--- src/server/entity/metadata/parrot_metadata.h
+++ src/server/entity/metadata/parrot_metadata.h
@@ -1,1 +1,16 @@
 #pragma once
+
+#include "tameable_animal_metadata.h"
+
+class ParrotMetadata : public TameableAnimalMetadata {
+public:
+    ParrotMetadata();
+    ~ParrotMetadata() override;
+
+public:
+    void wrapperWrite(MetadataBuffer&) override;
+
+public:
+    int variant = 0;
+
+};
