4b2c67758bfcbe1ad985c2164b8d8b81370de992
JNNGL <jnngles@gmail.com>
2022-07-31 13:26:33 +1100

    Goat metadata

--- src/server/entity/metadata/goat_metadata.cpp
+++ src/server/entity/metadata/goat_metadata.cpp
@@ -1,1 +1,11 @@
 #include "goat_metadata.h"
+
+GoatMetadata::GoatMetadata() = default;
+GoatMetadata::~GoatMetadata() = default;
+
+void GoatMetadata::wrapperWrite(MetadataBuffer& wrapper) {
+    AnimalMetadata::wrapperWrite(wrapper);
+    wrapper.writeBool(17, isScreaming);
+    wrapper.writeBool(18, hasLeftHorn);
+    wrapper.writeBool(19, hasRightHorn);
+}
--- src/server/entity/metadata/goat_metadata.h
+++ src/server/entity/metadata/goat_metadata.h
@@ -1,1 +1,18 @@
 #pragma once
+
+#include "animal_metadata.h"
+
+class GoatMetadata : public AnimalMetadata {
+public:
+    GoatMetadata();
+    ~GoatMetadata() override;
+
+public:
+    void wrapperWrite(MetadataBuffer&) override;
+
+public:
+    bool isScreaming = false;
+    bool hasLeftHorn = true;
+    bool hasRightHorn = true;
+
+};
