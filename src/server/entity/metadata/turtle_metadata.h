5e8dccd8a0a8fde9ae7645e3b76cdb9d33fdb61c
JNNGL <jnngles@gmail.com>
2022-07-31 11:50:53 +1100

    Turtle metadata

--- src/server/entity/metadata/turtle_metadata.h
+++ src/server/entity/metadata/turtle_metadata.h
@@ -1,1 +1,21 @@
 #pragma once
+
+#include "animal_metadata.h"
+
+class TurtleMetadata : public AnimalMetadata {
+public:
+    TurtleMetadata();
+    ~TurtleMetadata() override;
+
+public:
+    void wrapperWrite(MetadataBuffer&) override;
+
+public:
+    ProtocolPosition homePos {};
+    bool hasEgg = false;
+    bool isLayingEgg = false;
+    ProtocolPosition travelPos {};
+    bool isGoingHome = false;
+    bool isTraveling = false;
+
+};
