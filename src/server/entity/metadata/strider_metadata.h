5472b027e2f9f387616a9e8771930f06dddfec08
JNNGL <jnngles@gmail.com>
2022-07-31 12:09:04 +1100

    Strider metadata

--- src/server/entity/metadata/strider_metadata.h
+++ src/server/entity/metadata/strider_metadata.h
@@ -1,1 +1,18 @@
 #pragma once
+
+#include "animal_metadata.h"
+
+class StriderMetadata : public AnimalMetadata {
+public:
+    StriderMetadata();
+    ~StriderMetadata() override;
+
+public:
+    void wrapperWrite(MetadataBuffer&) override;
+
+public:
+    int boostTime = 0;
+    bool isShaking = false;
+    bool hasSaddle = false;
+
+};
