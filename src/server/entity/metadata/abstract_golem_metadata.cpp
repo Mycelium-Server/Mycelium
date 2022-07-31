70bda3def9584a021eb7c542a74e87b51e1926b5
JNNGL <jnngles@gmail.com>
2022-07-31 12:50:35 +1100

    Abstract golem metadata

--- src/server/entity/metadata/abstract_golem_metadata.cpp
+++ src/server/entity/metadata/abstract_golem_metadata.cpp
@@ -1,1 +1,8 @@
 #include "abstract_golem_metadata.h"
+
+AbstractGolemMetadata::AbstractGolemMetadata() = default;
+AbstractGolemMetadata::~AbstractGolemMetadata() = default;
+
+void AbstractGolemMetadata::wrapperWrite(MetadataBuffer& wrapper) {
+    PathfinderMobMetadata::wrapperWrite(wrapper);
+}
--- src/server/entity/metadata/abstract_golem_metadata.h
+++ src/server/entity/metadata/abstract_golem_metadata.h
@@ -1,1 +1,13 @@
 #pragma once
+
+#include "pathfinder_mob_metadata.h"
+
+class AbstractGolemMetadata : public PathfinderMobMetadata {
+public:
+    AbstractGolemMetadata();
+    ~AbstractGolemMetadata() override;
+
+public:
+    void wrapperWrite(MetadataBuffer&) override;
+
+};
