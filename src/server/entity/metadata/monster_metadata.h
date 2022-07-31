80657a567edbac74a9d149b89b93b4c45a290abe
JNNGL <jnngles@gmail.com>
2022-07-31 12:59:44 +1100

    Monster metadata

--- src/server/entity/metadata/monster_metadata.h
+++ src/server/entity/metadata/monster_metadata.h
@@ -1,1 +1,13 @@
 #pragma once
+
+#include "pathfinder_mob_metadata.h"
+
+class MonsterMetadata : public PathfinderMobMetadata {
+public:
+    MonsterMetadata();
+    ~MonsterMetadata() override;
+
+public:
+    void wrapperWrite(MetadataBuffer&) override;
+
+};
