60b41250c1b9ade9ae5021361ff269e918e2cf55
JNNGL <jnngles@gmail.com>
2022-07-31 12:00:36 +1100

    Mooshroom metadata

--- src/server/entity/metadata/mooshroom_metadata.cpp
+++ src/server/entity/metadata/mooshroom_metadata.cpp
@@ -1,1 +1,9 @@
 #include "mooshroom_metadata.h"
+
+MooshroomMetadata::MooshroomMetadata() = default;
+MooshroomMetadata::~MooshroomMetadata() = default;
+
+void MooshroomMetadata::wrapperWrite(MetadataBuffer& wrapper) {
+    CowMetadata::wrapperWrite(wrapper);
+    wrapper.writeString(17, variant);
+}
--- src/server/entity/metadata/mooshroom_metadata.h
+++ src/server/entity/metadata/mooshroom_metadata.h
@@ -1,1 +1,16 @@
 #pragma once
+
+#include "cow_metadata.h"
+
+class MooshroomMetadata : public CowMetadata {
+public:
+    MooshroomMetadata();
+    ~MooshroomMetadata() override;
+
+public:
+    void wrapperWrite(MetadataBuffer&) override;
+
+public:
+    std::string variant = "red"; // STRING????
+
+};
