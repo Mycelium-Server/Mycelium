b9a7407b7971105ed0379e4fbaaedd9ea9b9fb67
JNNGL <jnngles@gmail.com>
2022-07-31 11:45:07 +1100

    Pig metadata

--- src/server/entity/metadata/pig_metadata.cpp
+++ src/server/entity/metadata/pig_metadata.cpp
@@ -1,1 +1,10 @@
 #include "pig_metadata.h"
+
+PigMetadata::PigMetadata() = default;
+PigMetadata::~PigMetadata() = default;
+
+void PigMetadata::wrapperWrite(MetadataBuffer& wrapper) {
+    AnimalMetadata::wrapperWrite(wrapper);
+    wrapper.writeBool(17, hasSaddle);
+    wrapper.writeVarInt(18, boostTime);
+}
--- src/server/entity/metadata/pig_metadata.h
+++ src/server/entity/metadata/pig_metadata.h
@@ -1,1 +1,17 @@
 #pragma once
+
+#include "animal_metadata.h"
+
+class PigMetadata : public AnimalMetadata {
+public:
+    PigMetadata();
+    ~PigMetadata() override;
+
+public:
+    void wrapperWrite(MetadataBuffer&) override;
+
+public:
+    bool hasSaddle = false;
+    int boostTime = 0;
+
+};
