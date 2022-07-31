0187ced5984328ef5dfdbd47bdbac853e0a64094
JNNGL <jnngles@gmail.com>
2022-07-31 13:28:03 +1100

    Guardian metadata

--- src/server/entity/metadata/guardian_metadata.cpp
+++ src/server/entity/metadata/guardian_metadata.cpp
@@ -1,1 +1,10 @@
 #include "guardian_metadata.h"
+
+GuardianMetadata::GuardianMetadata() = default;
+GuardianMetadata::~GuardianMetadata() = default;
+
+void GuardianMetadata::wrapperWrite(MetadataBuffer& wrapper) {
+    MonsterMetadata::wrapperWrite(wrapper);
+    wrapper.writeBool(16, isRetractingSpikes);
+    wrapper.writeVarInt(17, targetEID);
+}
--- src/server/entity/metadata/guardian_metadata.h
+++ src/server/entity/metadata/guardian_metadata.h
@@ -1,1 +1,17 @@
 #pragma once
+
+#include "monster_metadata.h"
+
+class GuardianMetadata : public MonsterMetadata {
+public:
+    GuardianMetadata();
+    ~GuardianMetadata() override;
+
+public:
+    void wrapperWrite(MetadataBuffer&) override;
+
+public:
+    bool isRetractingSpikes = false;
+    int targetEID = 0;
+
+};
