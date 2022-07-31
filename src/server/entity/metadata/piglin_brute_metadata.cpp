8c80f857cfc700298bd722c648a053960af8f354
JNNGL <jnngles@gmail.com>
2022-07-31 13:04:29 +1100

    Piglin brute metadata

--- src/server/entity/metadata/piglin_brute_metadata.cpp
+++ src/server/entity/metadata/piglin_brute_metadata.cpp
@@ -1,1 +1,8 @@
 #include "piglin_brute_metadata.h"
+
+PiglinBruteMetadata::PiglinBruteMetadata() = default;
+PiglinBruteMetadata::~PiglinBruteMetadata() = default;
+
+void PiglinBruteMetadata::wrapperWrite(MetadataBuffer& wrapper) {
+    BasePiglinMetadata::wrapperWrite(wrapper);
+}
--- src/server/entity/metadata/piglin_brute_metadata.h
+++ src/server/entity/metadata/piglin_brute_metadata.h
@@ -1,1 +1,13 @@
 #pragma once
+
+#include "base_piglin_metadata.h"
+
+class PiglinBruteMetadata : public BasePiglinMetadata {
+public:
+    PiglinBruteMetadata();
+    ~PiglinBruteMetadata() override;
+
+public:
+    void wrapperWrite(MetadataBuffer&) override;
+
+};
