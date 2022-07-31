057e056c34f05411d61a92e5da183e74edf16fe0
JNNGL <jnngles@gmail.com>
2022-07-31 13:24:54 +1100

    Giant metadata

--- src/server/entity/metadata/giant_metadata.cpp
+++ src/server/entity/metadata/giant_metadata.cpp
@@ -1,1 +1,8 @@
 #include "giant_metadata.h"
+
+GiantMetadata::GiantMetadata() = default;
+GiantMetadata::~GiantMetadata() = default;
+
+void GiantMetadata::wrapperWrite(MetadataBuffer& wrapper) {
+    MonsterMetadata::wrapperWrite(wrapper);
+}
--- src/server/entity/metadata/giant_metadata.h
+++ src/server/entity/metadata/giant_metadata.h
@@ -1,1 +1,13 @@
 #pragma once
+
+#include "monster_metadata.h"
+
+class GiantMetadata : public MonsterMetadata {
+public:
+    GiantMetadata();
+    ~GiantMetadata() override;
+
+public:
+    void wrapperWrite(MetadataBuffer&) override;
+
+};
