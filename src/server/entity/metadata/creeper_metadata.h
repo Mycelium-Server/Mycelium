cf8d639d493521f1444ca125a03db7dfa3c16579
JNNGL <jnngles@gmail.com>
2022-07-31 13:22:37 +1100

    Creeper metadata

--- src/server/entity/metadata/creeper_metadata.h
+++ src/server/entity/metadata/creeper_metadata.h
@@ -1,1 +1,18 @@
 #pragma once
+
+#include "monster_metadata.h"
+
+class CreeperMetadata : public MonsterMetadata {
+public:
+    CreeperMetadata();
+    ~CreeperMetadata() override;
+
+public:
+    void wrapperWrite(MetadataBuffer&) override;
+
+public:
+    int state = -1;
+    bool isCharged = false;
+    bool isIgnited = false;
+
+};
