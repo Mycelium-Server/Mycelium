bf7cfa75761b9f174c7b43d97dea576d44b28ec5
JNNGL <jnngles@gmail.com>
2022-07-31 11:41:37 +1100

    Panda metadata

--- src/server/entity/metadata/panda_metadata.h
+++ src/server/entity/metadata/panda_metadata.h
@@ -1,1 +1,24 @@
 #pragma once
+
+#include "animal_metadata.h"
+
+class PandaMetadata : public AnimalMetadata {
+public:
+    PandaMetadata();
+    ~PandaMetadata() override;
+
+public:
+    void wrapperWrite(MetadataBuffer&) override;
+
+public:
+    int breedTimer = 0;
+    int sneezeTimer = 0;
+    int eatTimer = 0;
+    unsigned char mainGene = 0;
+    unsigned char hiddenGene = 0;
+    bool isSneezing = false;
+    bool isRolling = false;
+    bool isSitting = false;
+    bool isOnBack = false;
+
+};
