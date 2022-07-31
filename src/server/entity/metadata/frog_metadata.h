51c7210003cb22b961e261fc504a267564a8792f
JNNGL <jnngles@gmail.com>
2022-07-31 11:30:18 +1100

    Frog metadata

--- src/server/entity/metadata/frog_metadata.h
+++ src/server/entity/metadata/frog_metadata.h
@@ -1,1 +1,17 @@
 #pragma once
+
+#include "animal_metadata.h"
+
+class FrogMetadata : public AnimalMetadata {
+public:
+    FrogMetadata();
+    ~FrogMetadata() override;
+
+public:
+    void wrapperWrite(MetadataBuffer&) override;
+
+public:
+    FrogVariant variant = (FrogVariant) 0;
+    unsigned tongueTarget = 0;
+
+};
