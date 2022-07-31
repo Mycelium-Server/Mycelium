c05d3bd42d7775138f4fbf122dfbb11733406893
JNNGL <jnngles@gmail.com>
2022-07-31 11:20:51 +1100

    Fox metadata

--- src/server/entity/metadata/fox_metadata.cpp
+++ src/server/entity/metadata/fox_metadata.cpp
@@ -1,1 +1,18 @@
 #include "fox_metadata.h"
+
+FoxMetadata::FoxMetadata() = default;
+FoxMetadata::~FoxMetadata() = default;
+
+void FoxMetadata::wrapperWrite(MetadataBuffer& wrapper) {
+    AnimalMetadata::wrapperWrite(wrapper);
+    wrapper.writeVarInt(17, (int) isSnow);
+    wrapper.writeByte(18, isDefending   << 7 |
+                          isFaceplanted << 6 |
+                          isSleeping    << 5 |
+                          isPouncing    << 4 |
+                          isInterested  << 3 |
+                          isCrouching   << 2 |
+                          isSitting);
+    wrapper.writeOptUUID(19, firstUUID);
+    wrapper.writeOptUUID(20, secondUUID);
+}
--- src/server/entity/metadata/fox_metadata.h
+++ src/server/entity/metadata/fox_metadata.h
@@ -1,1 +1,25 @@
 #pragma once
+
+#include "animal_metadata.h"
+
+class FoxMetadata : public AnimalMetadata {
+public:
+    FoxMetadata();
+    ~FoxMetadata() override;
+
+public:
+    void wrapperWrite(MetadataBuffer&) override;
+
+public:
+    bool isSnow = false;
+    bool isSitting = false;
+    bool isCrouching = false;
+    bool isInterested = false;
+    bool isPouncing = false;
+    bool isSleeping = false;
+    bool isFaceplanted = false;
+    bool isDefending = false;
+    std::optional<uuids::uuid> firstUUID;
+    std::optional<uuids::uuid> secondUUID;
+
+};
