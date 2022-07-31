532633dfc3c3d9b85c3f898fded4362e2703b2ff
JNNGL <jnngles@gmail.com>
2022-07-31 13:03:22 +1100

    Piglin metadata

--- src/server/entity/metadata/piglin_metadata.h
+++ src/server/entity/metadata/piglin_metadata.h
@@ -1,1 +1,18 @@
 #pragma once
+
+#include "base_piglin_metadata.h"
+
+class PiglinMetadata : public BasePiglinMetadata {
+public:
+    PiglinMetadata();
+    ~PiglinMetadata() override;
+
+public:
+    void wrapperWrite(MetadataBuffer&) override;
+
+public:
+    bool isBaby = false;
+    bool isChargingCrossbow = false;
+    bool isDancing = false;
+
+};
