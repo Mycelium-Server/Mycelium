808f4d0f26830a4040b384f32c50548655ab3285
JNNGL <jnngles@gmail.com>
2022-07-31 12:49:13 +1100

    Wandering trader metadata

--- src/server/entity/metadata/wandering_trader_metadata.cpp
+++ src/server/entity/metadata/wandering_trader_metadata.cpp
@@ -1,1 +1,8 @@
 #include "wandering_trader_metadata.h"
+
+WanderingTraderMetadata::WanderingTraderMetadata() = default;
+WanderingTraderMetadata::~WanderingTraderMetadata() = default;
+
+void WanderingTraderMetadata::wrapperWrite(MetadataBuffer& wrapper) {
+    AbstractVillagerMetadata::wrapperWrite(wrapper);
+}
--- src/server/entity/metadata/wandering_trader_metadata.h
+++ src/server/entity/metadata/wandering_trader_metadata.h
@@ -1,1 +1,13 @@
 #pragma once
+
+#include "abstract_villager_metadata.h"
+
+class WanderingTraderMetadata : public AbstractVillagerMetadata {
+public:
+    WanderingTraderMetadata();
+    ~WanderingTraderMetadata() override;
+
+public:
+    void wrapperWrite(MetadataBuffer&) override;
+
+};
