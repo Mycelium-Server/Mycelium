4c20ff0fe24720d70ccc7d75c307818b3d4e8f60
JNNGL <jnngles@gmail.com>
2022-07-31 12:55:55 +1100

    Snow golem metadata

--- src/server/entity/metadata/snow_golem_metadata.cpp
+++ src/server/entity/metadata/snow_golem_metadata.cpp
@@ -1,1 +1,9 @@
 #include "snow_golem_metadata.h"
+
+SnowGolemMetadata::SnowGolemMetadata() = default;
+SnowGolemMetadata::~SnowGolemMetadata() = default;
+
+void SnowGolemMetadata::wrapperWrite(MetadataBuffer& wrapper) {
+    AbstractGolemMetadata::wrapperWrite(wrapper);
+    wrapper.writeByte(16, hasPumpkinHat ? 0x10 : 0x00);
+}
--- src/server/entity/metadata/snow_golem_metadata.h
+++ src/server/entity/metadata/snow_golem_metadata.h
@@ -1,1 +1,16 @@
 #pragma once
+
+#include "abstract_golem_metadata.h"
+
+class SnowGolemMetadata : public AbstractGolemMetadata {
+public:
+    SnowGolemMetadata();
+    ~SnowGolemMetadata() override;
+
+public:
+    void wrapperWrite(MetadataBuffer &) override;
+
+public:
+    bool hasPumpkinHat = true;
+
+};
