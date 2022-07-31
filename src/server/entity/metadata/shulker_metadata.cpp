4dcffa391de6fa780c68de15b75d8e8bc99b3d8e
JNNGL <jnngles@gmail.com>
2022-07-31 12:58:31 +1100

    Shulker metadata

--- src/server/entity/metadata/shulker_metadata.cpp
+++ src/server/entity/metadata/shulker_metadata.cpp
@@ -1,1 +1,12 @@
 #include "shulker_metadata.h"
+
+ShulkerMetadata::ShulkerMetadata() = default;
+ShulkerMetadata::~ShulkerMetadata() = default;
+
+void ShulkerMetadata::wrapperWrite(MetadataBuffer& wrapper) {
+    AbstractGolemMetadata::wrapperWrite(wrapper);
+    wrapper.writeDirection(16, attachFace);
+    wrapper.writeOptPosition(17, attachmentPosition);
+    wrapper.writeByte(18, shieldHeight);
+    wrapper.writeByte(19, color);
+}
--- src/server/entity/metadata/shulker_metadata.h
+++ src/server/entity/metadata/shulker_metadata.h
@@ -1,1 +1,19 @@
 #pragma once
+
+#include "abstract_golem_metadata.h"
+
+class ShulkerMetadata : public AbstractGolemMetadata {
+public:
+    ShulkerMetadata();
+    ~ShulkerMetadata() override;
+
+public:
+    void wrapperWrite(MetadataBuffer &) override;
+
+public:
+    Direction attachFace = Direction::DOWN;
+    std::optional<ProtocolPosition> attachmentPosition;
+    unsigned char shieldHeight = 0;
+    unsigned char color = 10;
+
+};
