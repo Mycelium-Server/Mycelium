ea0a0fb0426a325edd1c4f7edc20f9ea3ef3d40a
JNNGL <jnngles@gmail.com>
2022-07-31 13:01:24 +1100

    Base piglin metadata

--- src/server/entity/metadata/base_piglin_metadata.cpp
+++ src/server/entity/metadata/base_piglin_metadata.cpp
@@ -1,1 +1,9 @@
 #include "base_piglin_metadata.h"
+
+BasePiglinMetadata::BasePiglinMetadata() = default;
+BasePiglinMetadata::~BasePiglinMetadata() = default;
+
+void BasePiglinMetadata::wrapperWrite(MetadataBuffer& wrapper) {
+    MonsterMetadata::wrapperWrite(wrapper);
+    wrapper.writeBool(16, isImmuneToZombification);
+}
--- src/server/entity/metadata/base_piglin_metadata.h
+++ src/server/entity/metadata/base_piglin_metadata.h
@@ -1,1 +1,16 @@
 #pragma once
+
+#include "monster_metadata.h"
+
+class BasePiglinMetadata : public MonsterMetadata {
+public:
+    BasePiglinMetadata();
+    ~BasePiglinMetadata() override;
+
+public:
+    void wrapperWrite(MetadataBuffer&) override;
+
+public:
+    bool isImmuneToZombification = false;
+
+};
