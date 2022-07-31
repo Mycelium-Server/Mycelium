90f951114535023e390a97e4e9a9f3f8d35d5e4f
JNNGL <jnngles@gmail.com>
2022-07-31 12:21:27 +1100

    Cat metadata

--- src/server/entity/metadata/cat_metadata.h
+++ src/server/entity/metadata/cat_metadata.h
@@ -1,1 +1,20 @@
 #pragma once
+
+#include "tameable_animal_metadata.h"
+#include "cat_variant.h"
+
+class CatMetadata : public TameableAnimalMetadata {
+public:
+    CatMetadata();
+    ~CatMetadata() override;
+
+public:
+    void wrapperWrite(MetadataBuffer&) override;
+
+public:
+    CatVariant type = CatVariant::CAT_BLACK;
+    bool isLying = false;
+    bool isRelaxed = false;
+    int collarColor = 14;
+
+};
