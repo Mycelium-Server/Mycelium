a69979eb6697b1c67a1271cfcf2d1167cd140fc0
JNNGL <jnngles@gmail.com>
2022-08-04 22:01:28 +1100

    Pillager metadata

--- src/server/entity/metadata/pillager_metadata.cpp
+++ src/server/entity/metadata/pillager_metadata.cpp
@@ -1,1 +1,9 @@
 #include "pillager_metadata.h"
+
+PillagerMetadata::PillagerMetadata() = default;
+PillagerMetadata::~PillagerMetadata() = default;
+
+void PillagerMetadata::wrapperWrite(MetadataBuffer& wrapper) {
+  AbstractIllagerMetadata::wrapperWrite(wrapper);
+  wrapper.writeBool(17, isCharging);
+}
--- src/server/entity/metadata/pillager_metadata.h
+++ src/server/entity/metadata/pillager_metadata.h
@@ -1,1 +1,15 @@
 #pragma once
+
+#include "abstract_illager_metadata.h"
+
+class PillagerMetadata : public AbstractIllagerMetadata {
+ public:
+  PillagerMetadata();
+  ~PillagerMetadata() override;
+
+ public:
+  void wrapperWrite(MetadataBuffer&) override;
+
+ public:
+  bool isCharging = false;
+};
