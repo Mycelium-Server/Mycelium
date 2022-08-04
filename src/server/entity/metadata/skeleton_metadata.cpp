384d9ef559c574ea056dfa96fef64db16935b0f9
JNNGL <jnngles@gmail.com>
2022-08-04 22:14:53 +1100

    Skeleton metadata

--- src/server/entity/metadata/skeleton_metadata.cpp
+++ src/server/entity/metadata/skeleton_metadata.cpp
@@ -1,1 +1,8 @@
 #include "skeleton_metadata.h"
+
+SkeletonMetadata::SkeletonMetadata() = default;
+SkeletonMetadata::~SkeletonMetadata() = default;
+
+void SkeletonMetadata::wrapperWrite(MetadataBuffer& wrapper) {
+  AbstractSkeletonMetadata::wrapperWrite(wrapper);
+}
--- src/server/entity/metadata/skeleton_metadata.h
+++ src/server/entity/metadata/skeleton_metadata.h
@@ -1,1 +1,12 @@
 #pragma once
+
+#include "abstract_skeleton_metadata.h"
+
+class SkeletonMetadata : public AbstractSkeletonMetadata {
+ public:
+  SkeletonMetadata();
+  ~SkeletonMetadata() override;
+
+ public:
+  void wrapperWrite(MetadataBuffer&) override;
+};
