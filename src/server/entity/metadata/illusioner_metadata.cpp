f331c89c020503f54dfa0bb039532d781193bc02
JNNGL <jnngles@gmail.com>
2022-08-04 22:06:36 +1100

    Illusioner metadata

--- src/server/entity/metadata/illusioner_metadata.cpp
+++ src/server/entity/metadata/illusioner_metadata.cpp
@@ -1,1 +1,8 @@
 #include "illusioner_metadata.h"
+
+IllusionerMetadata::IllusionerMetadata() = default;
+IllusionerMetadata::~IllusionerMetadata() = default;
+
+void IllusionerMetadata::wrapperWrite(MetadataBuffer& wrapper) {
+  SpellcasterIllagerMetadata::wrapperWrite(wrapper);
+}
