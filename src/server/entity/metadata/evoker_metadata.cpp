b693609691bb0a5361eb156184d5e3efe6692df0
JNNGL <jnngles@gmail.com>
2022-08-04 22:04:55 +1100

    Evoker metadata

--- src/server/entity/metadata/evoker_metadata.cpp
+++ src/server/entity/metadata/evoker_metadata.cpp
@@ -1,1 +1,8 @@
 #include "evoker_metadata.h"
+
+EvokerMetadata::EvokerMetadata() = default;
+EvokerMetadata::~EvokerMetadata() = default;
+
+void EvokerMetadata::wrapperWrite(MetadataBuffer& wrapper) {
+  SpellcasterIllagerMetadata::wrapperWrite(wrapper);
+}
