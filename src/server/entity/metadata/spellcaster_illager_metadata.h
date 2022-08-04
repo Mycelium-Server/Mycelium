6a887d07ad81d2a785205c791a6b113e902c1703
JNNGL <jnngles@gmail.com>
2022-08-04 22:03:34 +1100

    Spellcaster illager metadata

--- src/server/entity/metadata/spellcaster_illager_metadata.h
+++ src/server/entity/metadata/spellcaster_illager_metadata.h
@@ -1,1 +1,16 @@
 #pragma once
+
+#include "abstract_illager_metadata.h"
+
+class SpellcasterIllagerMetadata : public AbstractIllagerMetadata {
+ public:
+  SpellcasterIllagerMetadata();
+  ~SpellcasterIllagerMetadata() override;
+
+ public:
+  void wrapperWrite(MetadataBuffer&) override;
+
+ public:
+  unsigned char spell = 0;
+
+};
