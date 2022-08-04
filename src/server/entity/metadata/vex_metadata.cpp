b365323a9707bc2f43bf56514d35bae20b0409b6
JNNGL <jnngles@gmail.com>
2022-08-04 22:12:16 +1100

    Vex metadata

--- src/server/entity/metadata/vex_metadata.cpp
+++ src/server/entity/metadata/vex_metadata.cpp
@@ -1,1 +1,9 @@
 #include "vex_metadata.h"
+
+VexMetadata::VexMetadata() = default;
+VexMetadata::~VexMetadata() = default;
+
+void VexMetadata::wrapperWrite(MetadataBuffer& wrapper) {
+  MonsterMetadata::wrapperWrite(wrapper);
+  wrapper.writeBool(16, isAttacking);
+}
