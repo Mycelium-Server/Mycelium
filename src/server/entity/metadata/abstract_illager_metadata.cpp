bfbd410df42d41ee3746aaccf34bfc84a061311a
JNNGL <jnngles@gmail.com>
2022-08-04 21:58:31 +1100

    Abstract illager metadata

--- src/server/entity/metadata/abstract_illager_metadata.cpp
+++ src/server/entity/metadata/abstract_illager_metadata.cpp
@@ -1,1 +1,8 @@
 #include "abstract_illager_metadata.h"
+
+AbstractIllagerMetadata::AbstractIllagerMetadata() = default;
+AbstractIllagerMetadata::~AbstractIllagerMetadata() = default;
+
+void AbstractIllagerMetadata::wrapperWrite(MetadataBuffer& wrapper) {
+  RaiderMetadata::wrapperWrite(wrapper);
+}
