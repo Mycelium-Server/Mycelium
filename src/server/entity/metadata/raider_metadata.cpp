a44c4ff8f1a17cdca91d54e5c8fe22cd6db1cfda
JNNGL <jnngles@gmail.com>
2022-08-04 21:52:59 +1100

    Raider metadata

--- src/server/entity/metadata/raider_metadata.cpp
+++ src/server/entity/metadata/raider_metadata.cpp
@@ -1,1 +1,9 @@
 #include "raider_metadata.h"
+
+RaiderMetadata::RaiderMetadata() = default;
+RaiderMetadata::~RaiderMetadata() = default;
+
+void RaiderMetadata::wrapperWrite(MetadataBuffer& wrapper) {
+  MonsterMetadata::wrapperWrite(wrapper);
+  wrapper.writeBool(16, isCelebrating);
+}
