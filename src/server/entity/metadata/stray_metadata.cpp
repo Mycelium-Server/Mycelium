9f363b52d982f972c469f7c80b374c600c937d16
JNNGL <jnngles@gmail.com>
2022-08-04 22:17:36 +1100

    Stray metadata

--- src/server/entity/metadata/stray_metadata.cpp
+++ src/server/entity/metadata/stray_metadata.cpp
@@ -1,1 +1,8 @@
 #include "stray_metadata.h"
+
+StrayMetadata::StrayMetadata() = default;
+StrayMetadata::~StrayMetadata() = default;
+
+void StrayMetadata::wrapperWrite(MetadataBuffer& wrapper) {
+  AbstractSkeletonMetadata::wrapperWrite(wrapper);
+}
--- src/server/entity/metadata/stray_metadata.h
+++ src/server/entity/metadata/stray_metadata.h
@@ -1,1 +1,12 @@
 #pragma once
+
+#include "abstract_skeleton_metadata.h"
+
+class StrayMetadata : public AbstractSkeletonMetadata {
+ public:
+  StrayMetadata();
+  ~StrayMetadata() override;
+
+ public:
+  void wrapperWrite(MetadataBuffer&) override;
+};
