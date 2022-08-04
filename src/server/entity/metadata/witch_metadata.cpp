33a6e168586bd15c163f2f0839d8834a40859479
JNNGL <jnngles@gmail.com>
2022-08-04 22:09:18 +1100

    Witch metadata

--- src/server/entity/metadata/witch_metadata.cpp
+++ src/server/entity/metadata/witch_metadata.cpp
@@ -1,1 +1,9 @@
 #include "witch_metadata.h"
+
+WitchMetadata::WitchMetadata() = default;
+WitchMetadata::~WitchMetadata() = default;
+
+void WitchMetadata::wrapperWrite(MetadataBuffer& wrapper) {
+  RaiderMetadata::wrapperWrite(wrapper);
+  wrapper.writeBool(17, isDrinkingPotion);
+}
