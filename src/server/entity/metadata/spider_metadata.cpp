87d998bb58c1d7facee0ad9c50f52fa05bf19e80
JNNGL <jnngles@gmail.com>
2022-08-04 22:19:26 +1100

    Spider metadata

--- src/server/entity/metadata/spider_metadata.cpp
+++ src/server/entity/metadata/spider_metadata.cpp
@@ -1,1 +1,9 @@
 #include "spider_metadata.h"
+
+SpiderMetadata::SpiderMetadata() = default;
+SpiderMetadata::~SpiderMetadata() = default;
+
+void SpiderMetadata::wrapperWrite(MetadataBuffer& wrapper) {
+  MonsterMetadata::wrapperWrite(wrapper);
+  wrapper.writeByte(16, isClimbing);
+}
--- src/server/entity/metadata/spider_metadata.h
+++ src/server/entity/metadata/spider_metadata.h
@@ -1,1 +1,15 @@
 #pragma once
+
+#include "monster_metadata.h"
+
+class SpiderMetadata : public MonsterMetadata {
+ public:
+  SpiderMetadata();
+  ~SpiderMetadata() override;
+
+ public:
+  void wrapperWrite(MetadataBuffer &) override;
+
+ public:
+  bool isClimbing = false;
+};
