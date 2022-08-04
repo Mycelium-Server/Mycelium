e53cc890780860a5e4c9630482427c5b97135906
JNNGL <jnngles@gmail.com>
2022-08-04 22:21:07 +1100

    Warden metadata

--- src/server/entity/metadata/warden_metadata.cpp
+++ src/server/entity/metadata/warden_metadata.cpp
@@ -1,1 +1,9 @@
 #include "warden_metadata.h"
+
+WardenMetadata::WardenMetadata() = default;
+WardenMetadata::~WardenMetadata() = default;
+
+void WardenMetadata::wrapperWrite(MetadataBuffer& wrapper) {
+  MonsterMetadata::wrapperWrite(wrapper);
+  wrapper.writeVarInt(16, angerLevel);
+}
