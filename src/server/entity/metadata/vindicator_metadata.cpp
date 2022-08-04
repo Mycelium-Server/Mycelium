6b4cc1beaf032a75d07e2d88cff0c6457df796a5
JNNGL <jnngles@gmail.com>
2022-08-04 21:59:52 +1100

    Vindicator metadata

--- src/server/entity/metadata/vindicator_metadata.cpp
+++ src/server/entity/metadata/vindicator_metadata.cpp
@@ -1,1 +1,8 @@
 #include "vindicator_metadata.h"
+
+VindicatorMetadata::VindicatorMetadata() = default;
+VindicatorMetadata::~VindicatorMetadata() = default;
+
+void VindicatorMetadata::wrapperWrite(MetadataBuffer& wrapper) {
+  AbstractIllagerMetadata::wrapperWrite(wrapper);
+}
