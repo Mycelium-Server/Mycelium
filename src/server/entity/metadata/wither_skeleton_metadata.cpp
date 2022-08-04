a93fad633173aa46b1629bf48a94bd83fc451187
JNNGL <jnngles@gmail.com>
2022-08-04 22:16:14 +1100

    Wither skeleton metadata

--- src/server/entity/metadata/wither_skeleton_metadata.cpp
+++ src/server/entity/metadata/wither_skeleton_metadata.cpp
@@ -1,1 +1,8 @@
 #include "wither_skeleton_metadata.h"
+
+WitherSkeletonMetadata::WitherSkeletonMetadata() = default;
+WitherSkeletonMetadata::~WitherSkeletonMetadata() = default;
+
+void WitherSkeletonMetadata::wrapperWrite(MetadataBuffer& wrapper) {
+  AbstractSkeletonMetadata::wrapperWrite(wrapper);
+}
