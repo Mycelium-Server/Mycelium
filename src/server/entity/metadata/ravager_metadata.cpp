8bd6005c9f4da1497b613b26f77dd83077573bbe
JNNGL <jnngles@gmail.com>
2022-08-04 22:08:01 +1100

    Ravager metadata

--- src/server/entity/metadata/ravager_metadata.cpp
+++ src/server/entity/metadata/ravager_metadata.cpp
@@ -1,1 +1,8 @@
 #include "ravager_metadata.h"
+
+RavagerMetadata::RavagerMetadata() = default;
+RavagerMetadata::~RavagerMetadata() = default;
+
+void RavagerMetadata::wrapperWrite(MetadataBuffer& wrapper) {
+  RaiderMetadata::wrapperWrite(wrapper);
+}
