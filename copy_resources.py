8433e7f2f15a23c89aa9c5b3ff0a5f7c477f23fd
JNNGL <jnngles@gmail.com>
2022-08-06 13:58:52 +1100

    [skip ci] Update readme

--- copy_resources.py
+++ copy_resources.py
@@ -2,7 +2,8 @@ import os
 import shutil
 import sys
 
-src = sys.argv[2]
+args_len = len(sys.argv)
+src = sys.argv[args_len - 1]
 
 def samefile(*args, **kwargs):
     return False
@@ -14,7 +15,7 @@ shutil.copy(src + "/properties.yml", "./properties.yml")
 shutil.copy(src + "/registry_codec.nbt", "./registry_codec.nbt")
 shutil.copy(src + "/cert.pem", "./cert.pem")
 if os.name == "nt":
-    lookup = [ "thirdparty/libuv/" + sys.argv[1] + "/libuv.dll", "thirdparty/libuv/" + sys.argv[1] + "/uv.dll",
+    lookup = [ "thirdparty/libuv/" + sys.argv[args_len - 2] + "/libuv.dll", "thirdparty/libuv/" + sys.argv[args_len - 2] + "/uv.dll",
                "thirdparty/libuv/libuv.dll", "thirdparty/libuv/uv.dll" ]
     for uv in lookup:
         if os.path.exists(uv):
