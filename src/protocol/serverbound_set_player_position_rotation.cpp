71b165503667989ea37ac96a0b42491d4958b85f
JNNGL <45935949+JNNGL@users.noreply.github.com>
2022-08-07 17:49:16 +1100

    Revert "Add license"
    This reverts commit e7c91f53fabfacfec7873c4c053520328d6f0a77.

--- src/protocol/serverbound_set_player_position_rotation.cpp
+++ src/protocol/serverbound_set_player_position_rotation.cpp
@@ -1,21 +1,3 @@
-/*
- * Mycelium
- * Copyright (C) 2022  JNNGL
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 #include "serverbound_set_player_position_rotation.h"
 
 #include "../listeners/play_packet_listener.h"
