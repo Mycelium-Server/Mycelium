af9d95969eb0eec5b4d793cd72f16850d12c7a80
JNNGL <jnngles@gmail.com>
2022-08-07 17:09:45 +1100

    Fix build

--- src/server/entity/metadata/enderman_metadata.cpp
+++ src/server/entity/metadata/enderman_metadata.cpp
@@ -5,7 +5,7 @@ EndermanMetadata::~EndermanMetadata() = default;
 
 void EndermanMetadata::wrapperWrite(MetadataBuffer& wrapper) {
   MonsterMetadata::wrapperWrite(wrapper);
-  wrapper.writeOptBlockID(16, carriedBlock)
+  wrapper.writeOptBlockID(16, carriedBlock);
   wrapper.writeBool(17, isScreaming);
   wrapper.writeBool(18, isStaring);
 }
