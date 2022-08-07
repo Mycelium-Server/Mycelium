46a0a9653b842be14115ba6b6441586d51660bf4
JNNGL <45935949+JNNGL@users.noreply.github.com>
2022-08-07 17:49:16 +1100

    Revert "Fix `copy_resources.py`"
    This reverts commit b8ee16f02be0072de704311fb7b1f13a9020152b.

--- copy_resources.py
+++ copy_resources.py
@@ -1,3 +1,4 @@
+import os
 import shutil
 import sys
 
@@ -13,3 +14,12 @@ shutil._samefile = samefile
 shutil.copy(src + "/properties.yml", "./properties.yml")
 shutil.copy(src + "/registry_codec.nbt", "./registry_codec.nbt")
 shutil.copy(src + "/cert.pem", "./cert.pem")
+if os.name == "nt":
+    lookup = [ "thirdparty/libuv/" + sys.argv[args_len - 2] + "/libuv.dll", "thirdparty/libuv/" + sys.argv[args_len - 2] + "/uv.dll",
+               "thirdparty/libuv/libuv.dll", "thirdparty/libuv/uv.dll" ]
+    for uv in lookup:
+        if os.path.exists(uv):
+            shutil.copy(uv, ".")
+            exit(0)
+
+    raise Exception("Couldn't find libuv.dll")
