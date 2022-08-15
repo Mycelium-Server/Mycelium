fb2488bb1cdac11e6b8f66f4ac994d9fd8f69610
JNNGL <jnngles@gmail.com>
2022-08-15 12:19:55 +1100

    Fix chiseled sandstone

--- src/server/items/chiseled_sandstone_item.cpp
+++ src/server/items/chiseled_sandstone_item.cpp
@@ -18,6 +18,8 @@
 
 #include "chiseled_sandstone_item.h"
 
+#include "../blocks/chiseled_sandstone_block.h"
+
 ChiseledSandstoneItem::ChiseledSandstoneItem() = default;
 ChiseledSandstoneItem::~ChiseledSandstoneItem() = default;
 
@@ -27,4 +29,8 @@ int ChiseledSandstoneItem::getID() const {
 
 std::shared_ptr<Item> ChiseledSandstoneItem::clone() const {
   return std::make_shared<ChiseledSandstoneItem>();
+}
+
+int ChiseledSandstoneItem::getBlockID(World *, const Vector3i &, const Vector3f &, const BlockFace &, const Vector3f &, bool) const {
+  return ChiseledSandstoneBlock().getId();
 }
